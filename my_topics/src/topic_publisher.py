8eb86e32d77092159887b6e9666b2098741056e3
git-sszy <yan.zhan@stmartin.edu>
2020-05-07 16:01:38 -0700

    yz_topic_publisher.py

--- my_topics/src/topic_publisher.py
+++ my_topics/src/topic_publisher.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
+
 import rospy
-from std_msgs.msg import Int32 # standard int
+from std_msgs.msg import Int32   # using rospy and std_msgs to execute the node
 
-# Setup: initialize node, register topic, set rate
-rospy.init_node( # initialize node
-  'topic_publisher' # node default name
+# initialize node
+rospy.init_node( 
+  'topic_publisher'              # publisher is set up as a default name
 )
-pub = rospy.Publisher( # register topic w/roscore
-  'counter', # topic name
-  Int32, # topic type
-  queue_size=5 # queue size
-)
-rate = rospy.Rate(2) # adaptive rate in Hz
 
-# Loop: publish, count, sleep
+# register a topic
+pub = rospy.Publisher( 
+  'counter',                     # topic name
+  Int32,                         # topic type int 32 from std_msgs
+  queue_size=5                   # queue size for keeping buffer, can't be too large
+)
+
+
+rate = rospy.Rate(2)             # sleep in a rate 2 in Hz
+
+# initializing the loop count is 0
 count = 0
-while not rospy.is_shutdown(): # until ctrl-c
-    pub.publish(count) # publish count
-    count += 1 # increment
-    rate.sleep() # set by rospy.Rate above
+
+# loop keeps runninng unless the rospy.is stops
+while not rospy.is_shutdown():   
+    pub.publish(count)           # publish function with a variable pub
+    count += 1                   # increment by 1
+    rate.sleep()                 # the loop weight the pospy.sleep according to the hz
